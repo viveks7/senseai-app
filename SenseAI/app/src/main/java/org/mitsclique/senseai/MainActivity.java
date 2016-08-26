3313726edc1d5efddbe3f2ae1cfe9624d965511f
viveks7 <vivek.mits@outlook.com>
2016-08-26 22:08:19 +0530

    Android app fro department techfest

---
+++ SenseAI/app/src/main/java/org/mitsclique/senseai/MainActivity.java
@@ -0,0 +1,35 @@
+package org.mitsclique.senseai;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(0xFFFFFFFF);
+        setSupportActionBar(toolbar);
+
+        if (findViewById(R.id.base_frame) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+            HomeFragment homeFragment = new HomeFragment();
+            homeFragment.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.base_frame, homeFragment).commit();
+        }
+
+    }
+}
